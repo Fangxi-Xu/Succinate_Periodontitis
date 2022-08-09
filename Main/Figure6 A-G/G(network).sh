ce15d275565c6e0ab7f563492006462a150d6b48
Fangxi <64149668+Fangxi-Xu@users.noreply.github.com>
2022-08-09 15:53:23 -0400

    Create G(network).sh

---
+++ Main/Figure6 A-G/G(network).sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#SBATCH --job-name=net
+#SBATCH --nodes=1 --ntasks-per-node=4
+#SBATCH --time=100:00:00   # HH/MM/SS
+#SBATCH --output=net_output
+#SBATCH --mem=64G
+
+module purge
+module load qiime/intel/1.9.1
+
+make_otu_network.py \
+-i ../biom_table_no_chloroplast/feature-table-no-chloroplast.biom \
+-m /scratch/fx363/succinate_perio_rerun/metadata.txt \
+-o otu_network
